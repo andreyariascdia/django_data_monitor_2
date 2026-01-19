01895571baeee27e0403379288519c4d3547e9b6
Alejandro Arias <andrauca@espol.edu.ec>
2026-01-19 04:18:49 +0000

    gunicorn

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'assets'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
